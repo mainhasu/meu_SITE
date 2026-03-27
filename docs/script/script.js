456a26fb5744e3f79a63733be27d038959ffd6f2
Rik <mainhasu9@gmail.com>
2026-03-27 14:54:23 -0300

    Update script.js

--- docs/script/script.js
+++ docs/script/script.js
@@ -1,3 +1,9 @@
+const corOriginal=document.body.style.backgroundColor;
+
 function alterarFundo(){
-  document.body.style.backgroundColor="ping";
+  document.body.style.backgroundColor="pink";
+}
+
+function voltarFundo(){
+  document.body.style.backgroundColor="corOriginal";
 }
